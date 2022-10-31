61129b2c8c8ab6bfc43aa947b1b1ae10281e655c
Djaila <bahsourga@yahoo.com>
2022-10-31 08:29:58 +0100

    init

---
+++ app/src/main/java/com/example/afro_frisoer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.afro_frisoer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
